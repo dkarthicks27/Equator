53741cabaeb372ba4aabab9bb208d075a796c6a9
dkarthicks27 <dkarthicks27@gmail.com>
2020-03-16 11:20:56 +0530

    commit

--- not_important/sample.py
+++ not_important/sample.py
@@ -1,1 +1,6 @@
-print('helo world')
+# this is classification
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfTransformer
+
+tfidf = TfidfTransformer(use_idf=True, sublinear_tf=True)
